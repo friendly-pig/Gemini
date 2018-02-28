1f0cb67623dac5e058dd373daf08a05a70591636
creozby <28669338+friendly-pig@users.noreply.github.com>
2018-02-28 11:15:35 +0300

    test poloniex dataframe

--- gemini/tests/test_poloniex.py
+++ gemini/tests/test_poloniex.py
@@ -1,3 +1,18 @@
-import pytest
 from gemini.helpers import poloniex
+import random
 
+pair = 'ETH_BTC'
+period = 1800
+days = 30
+control = ['close', 'high', 'low', 'open']
+
+
+def test_load_dataframe():
+    df = poloniex.load_dataframe(pair, period, days_history=days)
+    assert len(df) > 0
+    assert df.index[0] < df.index[-1]
+
+    columns_names = list(df)
+    for val in control:
+        assert val in columns_names
+        assert isinstance(df[val][random.randint(0, len(df))], float)
