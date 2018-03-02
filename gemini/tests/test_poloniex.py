421f6784df81c003d1e194b0637f8a18fe86cc11
creozby <28669338+friendly-pig@users.noreply.github.com>
2018-03-02 14:11:31 +0300

    add new test, improve code

--- gemini/tests/test_poloniex.py
+++ gemini/tests/test_poloniex.py
@@ -1,18 +1,17 @@
 from gemini.helpers import poloniex
-import random
-
-pair = 'ETH_BTC'
-period = 1800
-days = 30
-control = ['close', 'high', 'low', 'open']
+import numpy as np
 
 
 def test_load_dataframe():
-    df = poloniex.load_dataframe(pair, period, days_history=days)
+    df = poloniex.load_dataframe('ETH_BTC', 1800, days_history=30)
     assert len(df) > 0
     assert df.index[0] < df.index[-1]
 
-    columns_names = list(df)
-    for val in control:
-        assert val in columns_names
-        assert isinstance(df[val][random.randint(0, len(df))], float)
+    for val in ['close', 'high', 'low', 'open']:
+        assert val in df.columns.values
+        assert df.dtypes[val] == np.dtype('float64')
+
+
+def test_convert_pair_poloniex():
+    converted = poloniex.convert_pair_poloniex('ETH_BTC')
+    assert converted == 'BTC_ETH'
